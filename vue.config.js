f9cc1f6df8f7ba9946fd0658af59995fd4943c56
yangt57 <yangt57@chinaunicom.cn>
2021-01-28 13:44:05 +0800

    14生产地址

--- vue.config.js
+++ vue.config.js
@@ -37,12 +37,9 @@ module.exports = {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
         // target: `http://localhost:8080`,
-        //  target: `http://10.92.119.167:8081`,  //李奇本地
-        // target: `http://10.92.119.167:8081`,     //马儿本地
-        // target: `http://121.17.30.245:8081`,   //10服务器
-        //  target: `http://121.17.30.245:8081`,   //245衡水服务器
-        //   target: `http://121.17.30.248:8081`,   //248衡水服务器
-          target: `http://218.11.12.14:8081`,   //14衡水生产服务器
+        // target: `http://10.92.119.10:8081`,    //10服务器
+        // target: `http://121.17.30.248:8081`,    //248衡水服务器
+        target: `http://218.11.12.14:8081`,  //14衡水生产服务器
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
